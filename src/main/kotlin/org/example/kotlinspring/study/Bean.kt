e340a46be49bddb926d0b12c9b21d20b35f0fe6d
rhakdnj <chungjm0711@gmail.com>
2024-03-21 02:09:04 +0900

    feat: bean name

---
+++ src/main/kotlin/org/example/kotlinspring/study/Bean.kt
@@ -0,0 +1,42 @@
+package org.example.kotlinspring.study
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Config {
+  @Bean
+  fun name(): String {
+    return "Kotlin"
+  }
+
+  @Bean
+  fun age(): Int {
+    return 20
+  }
+
+  @Bean
+  fun person(): Person {
+    return Person(name(), age())
+  }
+
+  @Bean(name = ["customAddress"])
+  fun address(): Address {
+    return Address("firstLine", "city")
+  }
+}
+
+
+fun main() {
+  val context = AnnotationConfigApplicationContext(Config::class.java)
+
+  context.getBean("name").let(::println)
+  context.getBean("age").let(::println)
+  context.getBean("person").let(::println)
+  context.getBean("customAddress").let(::println)
+}
+
+
+data class Person(val name: String, val age: Int)
+data class Address(val firstLine: String, val city: String)
