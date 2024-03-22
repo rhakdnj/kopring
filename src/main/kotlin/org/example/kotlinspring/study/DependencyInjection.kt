ec654758c98b087c39e7d8de2f54a4e2aa6fa81c
rhakdnj <chungjm0711@gmail.com>
2024-03-22 23:45:31 +0900

    feat: scope

--- src/main/kotlin/org/example/kotlinspring/study/DependencyInjection.kt
+++ src/main/kotlin/org/example/kotlinspring/study/DependencyInjection.kt
@@ -1,9 +1,11 @@
 package org.example.kotlinspring.study
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.config.ConfigurableBeanFactory
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
 
 @Component
@@ -23,6 +25,7 @@ class FieldInjection {
 @Component
 class Dependency1
 
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @Component
 class Dependency2
 
@@ -35,4 +38,8 @@ fun main() {
   val context = AnnotationConfigApplicationContext(DependencyInjection::class.java)
 
   val fieldInjection = context.getBean(FieldInjection::class.java)
+  println(fieldInjection.dependency2)
+
+  val fieldInjection2 = context.getBean(FieldInjection::class.java)
+  println(fieldInjection2.dependency2)
 }
