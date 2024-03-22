5ef0e389cc88542c351afe4dee4be2cd0efed097
rhakdnj <chungjm0711@gmail.com>
2024-03-22 20:11:30 +0900

    feat: lazy bean 초기화

--- src/main/kotlin/org/example/kotlinspring/study/LazyInitialization.kt
+++ src/main/kotlin/org/example/kotlinspring/study/LazyInitialization.kt
@@ -29,9 +29,7 @@ fun main() {
   val context = AnnotationConfigApplicationContext(LazyInitialization::class.java)
 
   /**
-   * class B Bean 을 사용하지 않더라도 Eager Initialization of B 가 출력된다.
-   *
-   * Bean을 load하지도 않고 메서드를 호출하지 않더라도 자동으로 Bean이 초기화된다.
+   * 호출되는 시점의 @Bean 의 지연 초기화
    */
-  println("Context Created")
+  context.getBean(B::class.java).let(::println)
 }
