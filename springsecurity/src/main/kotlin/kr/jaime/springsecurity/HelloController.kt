bf78f82e12821c87d2f6a9d44c4bd25371cb839b
rhakdnj <chungjm0711@gmail.com>
2024-04-14 00:51:42 +0900

    feat: csrf (#15)

--- springsecurity/src/main/kotlin/kr/jaime/springsecurity/HelloController.kt
+++ springsecurity/src/main/kotlin/kr/jaime/springsecurity/HelloController.kt
@@ -1,5 +1,7 @@
 package kr.jaime.springsecurity
 
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.security.web.csrf.CsrfToken
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,4 +9,9 @@ import org.springframework.web.bind.annotation.RestController
 class HelloController {
   @GetMapping
   fun hello() = "Hello, world!"
+
+  @GetMapping("/csrf")
+  fun retrieveCsrfToken(request: HttpServletRequest): CsrfToken {
+    return request.getAttribute("_csrf") as CsrfToken
+  }
 }
