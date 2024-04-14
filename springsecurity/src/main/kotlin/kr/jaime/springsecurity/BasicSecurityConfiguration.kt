7b9217d9a7e0dfd7db10cd5005a80cef451eae4f
rhakdnj <chungjm0711@gmail.com>
2024-04-14 11:19:50 +0900

    feat: in memory user credentials (#15)

--- springsecurity/src/main/kotlin/kr/jaime/springsecurity/BasicSecurityConfiguration.kt
+++ springsecurity/src/main/kotlin/kr/jaime/springsecurity/BasicSecurityConfiguration.kt
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -40,5 +42,20 @@ class BasicSecurityConfiguration {
             .allowedOrigins("http://localhost:3000")
       }
     }
+  }
+
+  @Bean
+  fun userDetailsService(): InMemoryUserDetailsManager {
+    val user = User.withUsername("user")
+        .password("{noop}user")
+        .roles("USER")
+        .build()
+
+    val admin = User.withUsername("admin")
+        .password("{noop}admin")
+        .roles("ADMIN")
+        .build()
+
+    return InMemoryUserDetailsManager(user, admin)
   }
 }
